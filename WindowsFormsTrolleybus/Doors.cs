9941fa111846a6704d557bb649654d07125c4114
Мария <uba2014@mail.ru>
2019-10-13 22:52:03 +0400

    Добавлен класс,в котором находится алгоритм добавления и удаления дверей

---
+++ WindowsFormsTrolleybus/Doors.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsTrolleybus
+{
+    class Doors
+    {
+
+        int count = 1;
+
+        public void AddDoor()
+        {
+            if (count < 3)
+            {
+                count++;
+            }
+        }
+        public void DeleteDoor()
+        {
+            if (count > 1)
+            {
+                count--;
+            }
+        }
+
+        public void DrawDoors(Graphics g, float _startPosX, float _startPosY)
+        {
+            Pen blackpen = new Pen(Color.Black);
+            Brush darkblueBrush = new SolidBrush(Color.DarkBlue);
+            int sizedoor = 8;
+
+            for (int i = 0; i < count; i++)
+            {
+                g.DrawRectangle(blackpen, _startPosX + 26 + 2 * sizedoor * i, _startPosY + 17, 5, 28);
+                g.FillRectangle(darkblueBrush, _startPosX + 26 + 2 * sizedoor * i, _startPosY + 17, 5, 28);
+            }
+
+            //switch (count)
+            //{
+            //    case 1:
+            //        g.DrawRectangle(blackpen, _startPosX + 26, _startPosY + 17, 5, 28);
+            //        g.FillRectangle(darkblueBrush, _startPosX + 26, _startPosY + 17, 5, 28);
+            //        break;
+            //    case 2:
+            //        g.DrawRectangle(blackpen, _startPosX + 26, _startPosY + 17, 5, 28);
+            //        g.FillRectangle(darkblueBrush, _startPosX + 26, _startPosY + 17, 5, 28);
+
+            //        g.DrawRectangle(blackpen, _startPosX + 42, _startPosY + 17, 5, 28);
+            //        g.FillRectangle(darkblueBrush, _startPosX + 42, _startPosY + 17, 5, 28);
+            //        break;
+            //    case 3:
+            //        g.DrawRectangle(blackpen, _startPosX + 26, _startPosY + 17, 5, 28);
+            //        g.FillRectangle(darkblueBrush, _startPosX + 26, _startPosY + 17, 5, 28);
+
+            //        g.DrawRectangle(blackpen, _startPosX + 42, _startPosY + 17, 5, 28);
+            //        g.FillRectangle(darkblueBrush, _startPosX + 42, _startPosY + 17, 5, 28);
+
+            //        g.DrawRectangle(blackpen, _startPosX + 58, _startPosY + 17, 5, 28);
+            //        g.FillRectangle(darkblueBrush, _startPosX + 58, _startPosY + 17, 5, 28);
+            //        break;
+            //}         
+
+        }
+    }
+}
--- WindowsFormsTrolleybus/FormTrolleybus.Designer.cs
+++ WindowsFormsTrolleybus/FormTrolleybus.Designer.cs
@@ -3,14 +3,14 @@
     partial class FormTrolleybus
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,28 +20,145 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBoxTrolleybus = new System.Windows.Forms.PictureBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonAddDoor = new System.Windows.Forms.Button();
+            this.buttonDeleteDoor = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTrolleybus)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBoxTrolleybus
+            // 
+            this.pictureBoxTrolleybus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxTrolleybus.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxTrolleybus.Name = "pictureBoxTrolleybus";
+            this.pictureBoxTrolleybus.Size = new System.Drawing.Size(882, 453);
+            this.pictureBoxTrolleybus.TabIndex = 11;
+            this.pictureBoxTrolleybus.TabStop = false;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(31, 29);
+            this.buttonCreate.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(100, 28);
+            this.buttonCreate.TabIndex = 12;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = global::WindowsFormsTrolleybus.Properties.Resources.up;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(787, 358);
+            this.buttonUp.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(40, 37);
+            this.buttonUp.TabIndex = 13;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = global::WindowsFormsTrolleybus.Properties.Resources.left;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(739, 403);
+            this.buttonLeft.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(40, 37);
+            this.buttonLeft.TabIndex = 14;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.UseWaitCursor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = global::WindowsFormsTrolleybus.Properties.Resources.down;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(787, 403);
+            this.buttonDown.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(40, 37);
+            this.buttonDown.TabIndex = 15;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = global::WindowsFormsTrolleybus.Properties.Resources.right;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(835, 403);
+            this.buttonRight.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(40, 37);
+            this.buttonRight.TabIndex = 16;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonAddDoor
+            // 
+            this.buttonAddDoor.Location = new System.Drawing.Point(560, 369);
+            this.buttonAddDoor.Name = "buttonAddDoor";
+            this.buttonAddDoor.Size = new System.Drawing.Size(134, 36);
+            this.buttonAddDoor.TabIndex = 17;
+            this.buttonAddDoor.Text = "Добавить дверь";
+            this.buttonAddDoor.Click += new System.EventHandler(this.buttonAddDoor_Click);
+            // 
+            // buttonDeleteDoor
+            // 
+            this.buttonDeleteDoor.Location = new System.Drawing.Point(560, 411);
+            this.buttonDeleteDoor.Name = "buttonDeleteDoor";
+            this.buttonDeleteDoor.Size = new System.Drawing.Size(134, 37);
+            this.buttonDeleteDoor.TabIndex = 18;
+            this.buttonDeleteDoor.Text = "Удалить дверь";
+            this.buttonDeleteDoor.Click += new System.EventHandler(this.buttonDeleteDoor_Click);
             // 
             // FormTrolleybus
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.buttonDeleteDoor);
+            this.Controls.Add(this.buttonAddDoor);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.pictureBoxTrolleybus);
             this.Name = "FormTrolleybus";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Троллейбус";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTrolleybus)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.PictureBox pictureBoxTrolleybus;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonAddDoor;
+        private System.Windows.Forms.Button buttonDeleteDoor;
     }
 }
-
