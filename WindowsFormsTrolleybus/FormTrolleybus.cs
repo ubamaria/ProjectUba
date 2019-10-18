968b34903bcea9afaa6f816d8416528713529e07
Мария <uba2014@mail.ru>
2019-10-18 09:55:03 +0400

    классная работа 1

--- WindowsFormsTrolleybus/FormTrolleybus.cs
+++ WindowsFormsTrolleybus/FormTrolleybus.cs
@@ -12,11 +12,47 @@ namespace WindowsFormsTrolleybus
 {
     public partial class FormTrolleybus : Form
     {
+        private Trolleybus trolleybus;
+
         public FormTrolleybus()
         {
             InitializeComponent();
         }
 
-        
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxTrolleybus.Width, pictureBoxTrolleybus.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            trolleybus.DrawTrolleybus(gr);
+            pictureBoxTrolleybus.Image = bmp;
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            trolleybus = new Trolleybus(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue, Color.Yellow, true, true, true);
+            trolleybus.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxTrolleybus.Width, pictureBoxTrolleybus.Height);
+            Draw();
+        }
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUp":
+                    trolleybus.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDown":
+                    trolleybus.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLeft":
+                    trolleybus.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRight":
+                    trolleybus.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
     }
 }
