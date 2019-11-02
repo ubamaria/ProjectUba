5aa9282e1e7f86036d06939d47ccd7c4585d6726
Мария <uba2014@mail.ru>
2019-11-02 12:30:39 +0400

    cw4

--- WindowsFormsTrolleybus/BusStation.cs
+++ WindowsFormsTrolleybus/BusStation.cs
@@ -10,9 +10,14 @@ namespace WindowsFormsTrolleybus
     public class BusStation<T> where T : class, ITransport
     {
         /// <summary>
-        /// Массив объектов, которые храним
+        /// Словарь
         /// </summary>
-        private T[] _places;
+        private Dictionary<int, T> _places;
+
+        /// <summary>
+        /// Максимальное количество мест на стоянке
+        /// </summary>
+        private int _maxCount;
 
         /// <summary>
         /// Ширина окна отрисовки
@@ -41,29 +46,30 @@ namespace WindowsFormsTrolleybus
         /// <param name="pictureHeight">Размер автовокзала - высота</param>
         public BusStation(int sizes, int pictureWidth, int pictureHeight)
         {
-            _places = new T[sizes];
+            _maxCount = sizes;
+            _places = new Dictionary<int, T>();
             PictureWidth = pictureWidth;
             PictureHeight = pictureHeight;
-            for (int i = 0; i < _places.Length; i++)
-            {
-                _places[i] = null;
-            }
         }
 
         /// <summary>
         /// Перегрузка оператора сложения
-        /// Логика действия: на парковку добавляется автомобиль
+        /// Логика действия: на стоянку добавляется автобус
         /// </summary>
         /// <param name="p">Парковка</param>
-        /// <param name="car">Добавляемый автомобиль</param>
+        /// <param name="trolleybus">Добавляемый автобус</param>
         /// <returns></returns>
         public static int operator +(BusStation<T> p, T trolleybus)
         {
-            for (int i = 0; i < p._places.Length; i++)
+            if (p._places.Count == p._maxCount)
+            {
+                return -1;
+            }
+            for (int i = 0; i < p._maxCount; i++)
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p._places[i] = trolleybus;
+                    p._places.Add(i, trolleybus);
                     p._places[i].SetPosition(5 + i / 5 * _placeSizeWidth + 5,
                      i % 5 * _placeSizeHeight + 15, p.PictureWidth,
                     p.PictureHeight);
@@ -83,14 +89,10 @@ namespace WindowsFormsTrolleybus
         /// <returns></returns>
         public static T operator -(BusStation<T> p, int index)
         {
-            if (index < 0 || index > p._places.Length)
-            {
-                return null;
-            }
             if (!p.CheckFreePlace(index))
             {
                 T trolleybus = p._places[index];
-                p._places[index] = null;
+                p._places.Remove(index);
                 return trolleybus;
             }
             return null;
@@ -104,7 +106,7 @@ namespace WindowsFormsTrolleybus
         /// <returns></returns>
         private bool CheckFreePlace(int index)
         {
-            return _places[index] == null;
+            return !_places.ContainsKey(index);
         }
 
         /// <summary>
@@ -114,13 +116,12 @@ namespace WindowsFormsTrolleybus
         public void Draw(Graphics g)
         {
             DrawMarking(g);
-            for (int i = 0; i < _places.Length; i++)
+            var keys = _places.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (!CheckFreePlace(i))
-                {//если место не пустое
-                    _places[i].DrawBus(g);
-                }
+                _places[keys[i]].DrawBus(g);
             }
+
         }
         /// <summary>
         /// Метод отрисовки разметки парковочных мест
@@ -130,11 +131,11 @@ namespace WindowsFormsTrolleybus
         {
             Pen pen = new Pen(Color.Black, 3);
             //границы праковки
-            g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
-            for (int i = 0; i < _places.Length / 5; i++)
+            g.DrawRectangle(pen, 0, 0, (_maxCount / 5) * _placeSizeWidth, 480);
+            for (int i = 0; i < _maxCount / 5; i++)
             {//отрисовываем, по 5 мест на линии
                 for (int j = 0; j < 6; ++j)
-                {//линия рамзетки места
+                {//линия разметки места
                     g.DrawLine(pen, i * _placeSizeWidth, j * _placeSizeHeight,
                     i * _placeSizeWidth + 110, j * _placeSizeHeight);
                 }
--- WindowsFormsTrolleybus/FormBusStation.Designer.cs
+++ WindowsFormsTrolleybus/FormBusStation.Designer.cs
@@ -32,11 +32,13 @@
             this.buttonSetBus = new System.Windows.Forms.Button();
             this.buttonSetTrolleybus = new System.Windows.Forms.Button();
             this.maskedTextBox = new System.Windows.Forms.GroupBox();
+            this.buttonTakeBus = new System.Windows.Forms.Button();
             this.pictureBoxTakeBus = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.buttonTakeBus = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxLevels = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStation)).BeginInit();
             this.maskedTextBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeBus)).BeginInit();
@@ -53,7 +55,7 @@
             // 
             // buttonSetBus
             // 
-            this.buttonSetBus.Location = new System.Drawing.Point(1167, 12);
+            this.buttonSetBus.Location = new System.Drawing.Point(1167, 167);
             this.buttonSetBus.Name = "buttonSetBus";
             this.buttonSetBus.Size = new System.Drawing.Size(133, 59);
             this.buttonSetBus.TabIndex = 1;
@@ -63,7 +65,7 @@
             // 
             // buttonSetTrolleybus
             // 
-            this.buttonSetTrolleybus.Location = new System.Drawing.Point(1167, 107);
+            this.buttonSetTrolleybus.Location = new System.Drawing.Point(1167, 254);
             this.buttonSetTrolleybus.Name = "buttonSetTrolleybus";
             this.buttonSetTrolleybus.Size = new System.Drawing.Size(133, 59);
             this.buttonSetTrolleybus.TabIndex = 2;
@@ -84,6 +86,16 @@
             this.maskedTextBox.TabIndex = 3;
             this.maskedTextBox.TabStop = false;
             // 
+            // buttonTakeBus
+            // 
+            this.buttonTakeBus.Location = new System.Drawing.Point(33, 86);
+            this.buttonTakeBus.Name = "buttonTakeBus";
+            this.buttonTakeBus.Size = new System.Drawing.Size(96, 32);
+            this.buttonTakeBus.TabIndex = 7;
+            this.buttonTakeBus.Text = "Забрать";
+            this.buttonTakeBus.UseVisualStyleBackColor = true;
+            this.buttonTakeBus.Click += new System.EventHandler(this.buttonTakeBus_Click);
+            // 
             // pictureBoxTakeBus
             // 
             this.pictureBoxTakeBus.Location = new System.Drawing.Point(0, 143);
@@ -91,15 +103,6 @@
             this.pictureBoxTakeBus.Size = new System.Drawing.Size(169, 90);
             this.pictureBoxTakeBus.TabIndex = 0;
             this.pictureBoxTakeBus.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 17);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Забрать автобус";
             // 
             // maskedTextBox1
             // 
@@ -116,23 +119,42 @@
             this.label2.Size = new System.Drawing.Size(53, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "Место:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // buttonTakeBus
+            // label1
             // 
-            this.buttonTakeBus.Location = new System.Drawing.Point(33, 86);
-            this.buttonTakeBus.Name = "buttonTakeBus";
-            this.buttonTakeBus.Size = new System.Drawing.Size(96, 32);
-            this.buttonTakeBus.TabIndex = 7;
-            this.buttonTakeBus.Text = "Забрать";
-            this.buttonTakeBus.UseVisualStyleBackColor = true;
-            this.buttonTakeBus.Click += new System.EventHandler(this.buttonTakeBus_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Забрать автобус";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1201, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Уровни";
+            // 
+            // listBoxLevels
+            // 
+            this.listBoxLevels.FormattingEnabled = true;
+            this.listBoxLevels.ItemHeight = 16;
+            this.listBoxLevels.Location = new System.Drawing.Point(1161, 35);
+            this.listBoxLevels.Name = "listBoxLevels";
+            this.listBoxLevels.Size = new System.Drawing.Size(156, 116);
+            this.listBoxLevels.TabIndex = 5;
+            this.listBoxLevels.SelectedIndexChanged += new System.EventHandler(this.listBoxLevels_SelectedIndexChanged);
             // 
             // FormBusStation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1332, 615);
+            this.Controls.Add(this.listBoxLevels);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.maskedTextBox);
             this.Controls.Add(this.buttonSetTrolleybus);
             this.Controls.Add(this.buttonSetBus);
@@ -144,6 +166,7 @@
             this.maskedTextBox.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeBus)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -158,5 +181,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBoxTakeBus;
         private System.Windows.Forms.Button buttonTakeBus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxLevels;
     }
 }
